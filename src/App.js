568f3298dee513a4cd095807b4bd0009f5343ac1
HauTC <hautc@runsyustem.net>
2023-11-23 10:07:27 +0700

    learning react in F8 fullstack

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://fullstack.edu.vn"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in F8 fullstack course
         </a>
       </header>
     </div>
