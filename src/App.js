5ab7a3310aa1b16363f52ec85711cc53adb178e7
HauTC <hautc@runsyustem.net>
2023-11-28 11:32:59 +0700

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { ReactDOM, useState } from "react";
 
+const gifts = ["🍎", "🍊", "🍋", "🍌", "🍉", "🍇"];
 function App() {
+  const handleGetGift = () => {
+    const index = Math.floor(Math.random() * gifts.length);
+    setGift(gifts[index]);
+  };
+  const [gift, setGift] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://fullstack.edu.vn"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React in F8 fullstack course
-        </a>
-      </header>
+    <div className="App" style={{ padding: 32 }}>
+      <h1>{gift || "don't have gift"}</h1>
+      <button onClick={handleGetGift} style={{ padding: 16 }}>
+        Get Gift
+      </button>
     </div>
   );
 }
-
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import React from "react";
 
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  let h1 = React.createElement("h1", null, "Hello world");
+  ReactDOM.render(h1, document.getElementById("root"));
 });
